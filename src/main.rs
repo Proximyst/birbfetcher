b91ae88a28a821f8f3adf06ff9456806e0875f9c
Mariell Hoversholm <proximyst@proximyst.com>
2020-07-26 16:47:59 +0200

    Add licence headers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// birbfetcher - Collect bird images with ease.
+// Copyright (C) 2020 Mariell Hoversholm
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 mod discord;
 mod error;
 mod http;
@@ -47,6 +63,13 @@ pub static REQWEST_CLIENT: Lazy<ReqwestClient> = Lazy::new(|| {
 
 #[tokio::main]
 async fn main() {
+    eprintln!(concat!(env!("CARGO_PKG_NAME"), " (v", env!("CARGO_PKG_VERSION"), ")"));
+    eprintln!(r#"
+birbfetcher Copyright (C) 2020 Mariell Hoversholm
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions.
+"#);
     match err_main().await {
         Ok(()) => return,
         Err(e) => {
